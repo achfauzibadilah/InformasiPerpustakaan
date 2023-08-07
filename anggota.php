c93e22213f38aadfae98e3876cd5d6a81ef71527
Achmad Fauzi <86862129+achfauzibadilah@users.noreply.github.com>
2023-08-07 14:38:45 +0700

    update tampilan anggota

--- anggota.php
+++ anggota.php
@@ -1,36 +1,74 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sistem Informasi Perpustakaan - Anggota</title>
-    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <title>Sistem Informasi Perpustakaan</title>
+    <link href="css/styles.css" rel="stylesheet" />
+    <link href="css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/js/all.min.js" crossorigin="anonymous"></script>
+
 </head>
-<body>
-<div class="container mt-5">
-    <div class="row">
-        <div class="col-md-8 offset-md-2">
-            <div class="card">
-                <div class="card-header text-center">
-                    <h1>Sistem Informasi Perpustakaan</h1>
-                </div>
-            </div>
-        </div>
-            <div class="row mt-3">
-                <div class="col-md-2 text-center">
-                    <ul class="list-group">
-                        <li class="list-group-item"><a href="./" class="text-decoration-none">Home</a></li>
-                        <li class="list-group-item"><a href="anggota.php" class="text-decoration-none">Anggota</a></li>
-                        <li class="list-group-item"><a href="buku.php" class="text-decoration-none">Buku</a></li>
-                        <li class="list-group-item"><a href="pinjam.php" class="text-decoration-none">Pinjam</a></li>
+<body class="sb-nav-fixed">
+        <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
+            <a class="navbar-brand" href="index.php">HOME</a>
+            <button class="btn btn-link btn-sm order-1 order-lg-0" id="sidebarToggle" href="#"><i class="fas fa-bars"></i></button>
+            <!-- Navbar Search-->
+            <form class="d-none d-md-inline-block form-inline ml-auto mr-0 mr-md-3 my-2 my-md-0">
+                <div class="input-group">
+                    <ul class="navbar-nav ml-auto ml-md-0">
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" id="userDropdown" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
+                                <!-- <div class="dropdown-divider"></div> -->
+                                <a class="dropdown-item" href="logout.php">Logout</a>
+                            </div>
+                        </li>
                     </ul>
                 </div>
-                <div class="col-md-10">
-                    <div class="d-flex justify-content-end mb-3">
-                        <a href="input_anggota.php" class="btn btn-primary">Tambah Anggota</a>
+            </form>
+        </nav>
+        <div id="layoutSidenav">
+            <div id="layoutSidenav_nav">
+                <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+                    <div class="sb-sidenav-menu">
+                        <div class="nav">
+                            <div class="sb-sidenav-menu-heading">Menu Utama</div>
+                            <a class="nav-link" href="anggota.php">
+                                Anggota
+                            </a>
+                            <a class="nav-link" href="buku.php">
+                                Buku
+                            </a>
+                            <a class="nav-link" href="pinjam.php">
+                                Pinjam
+                            </a>
+                            
+                        </div>
                     </div>
-                        <div class="card text-center">
-                            <div class="card-body">
+                    <!-- <div class="sb-sidenav-footer">
+                        <div class="small">Logged in as:</div>
+                        Not Yet Setup
+                    </div> -->
+                    <div>
+                    <a class="nav-link" href="logout.php ">
+                        Logout
+                    </a>
+                    </div>
+                </nav>
+            </div>
+            <div id="layoutSidenav_content">
+                <main>
+                    <div class="container-fluid">
+                        <h1 class="mt-4 text-center">Sistem Informasi Perpustakaan</h1>
+                        <!-- <ol class="breadcrumb mb-4">
+                            <li class="breadcrumb-item active">Dashboard</li>
+                        </ol> -->
+                        <div class="card mb-4">
+                            <div class="card-header">
+                            <a href="input_anggota.php" class="btn btn-dark">Tambah Anggota</a>
+                            </div>
+                            <div class="card-body text-center">
                                 <table class="table table-bordered">
                                     <thead>
                                         <tr>
@@ -44,8 +82,8 @@
                                         </tr>
                                     </thead>
                                     <tbody>
-                                        <?php 
-                                        include "koneksi.php";
+                                        <?php
+                                        require 'koneksi.php';
                                         $query = "SELECT * FROM anggota ORDER BY id_anggota";
                                         $sql = mysqli_query($koneksi, $query);
                                         $no = 1;
@@ -71,11 +109,33 @@
                             <div class="card-footer text-center">
                                 Kelompok 5
                             </div>
-                        </div>        
-                    </div>  
+                        </div>
+
+                    </div>
+                </main>
+                <footer class="py-4 bg-light mt-auto">
+                    <div class="container-fluid">
+                        <div class="d-flex align-items-center justify-content-between small">
+                            <div class="text-muted">Copyright &copy; Tugas Web 2 - Kelompok 5</div>
+                            <div>
+                                <a href="#">Privacy Policy</a>
+                                &middot;
+                                <a href="#">Terms &amp; Conditions</a>
+                            </div>
+                        </div>
+                    </div>
+                </footer>
             </div>
-    </div>
-</div>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+        </div>
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+        <script src="js/scripts.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
+        <script src="assets/demo/chart-area-demo.js"></script>
+        <script src="assets/demo/chart-bar-demo.js"></script>
+        <script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js" crossorigin="anonymous"></script>
+        <script src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js" crossorigin="anonymous"></script>
+        <script src="assets/demo/datatables-demo.js"></script>
+<script src="js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
